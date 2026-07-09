7153ddef9f8798b24e4c9c53ba6457bb57418b88
kobadaidesu <daigokobayashi04@gmail.com>
2026-07-09 21:58:56 +0900

    Buttons and State

---
+++ DiceRoller/DiceRoller/ContentView.swift
@@ -0,0 +1,54 @@
+//
+//  ContentView.swift
+//  DiceRoller
+//
+//  Created by Kobayashi Daigo on 2026/07/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var numberOfDice: Int = 1
+    
+    var body: some View {
+        VStack {
+            Text("Dice Roller")
+                .font(.largeTitle.lowercaseSmallCaps())
+                .foregroundStyle(.white)
+            
+            HStack {
+                ForEach(1...numberOfDice, id: \.description) { _ in
+                    DiceView()
+                }
+            }
+            
+            HStack {
+                Button("Remove Dice", systemImage: "minus.circle.fill") {
+                    withAnimation {
+                        numberOfDice -= 1
+                    }
+                }
+                .disabled(numberOfDice == 1)
+                
+                Button("Add Dice", systemImage: "plus.circle.fill") {
+                    withAnimation {
+                        numberOfDice += 1
+                    }
+                }
+                .disabled(numberOfDice == 5)
+            }
+            .padding()
+            .labelStyle(.iconOnly)
+            .font(.title)
+        }
+        .padding()
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color("AppBackground"))
+        .tint(.white)
+    }
+}
+
+
+#Preview {
+    ContentView()
+}
---
+++ DiceRoller/DiceRoller/DiceRollerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRollerApp.swift
+//  DiceRoller
+//
+//  Created by Kobayashi Daigo on 2026/07/09.
+//
+
+import SwiftUI
+
+@main
+struct DiceRollerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
